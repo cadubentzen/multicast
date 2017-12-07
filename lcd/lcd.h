70ea8301df56fcc872689995f4202d2885911ebd
cadubentzen <cadubentzen@gmail.com>
2017-12-07 07:45:44 -0300

    Implementing LCD for Beagle. Needs to be tested

--- lcd/lcd.h
+++ lcd/lcd.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifdef BEAGLE
+    #define LCD_BUF_SIZE 100
+#endif
+
 class LCD {
 public:
     static LCD* singleton() {
@@ -15,4 +19,14 @@ public:
 private:
     static LCD *m_lcd;
     LCD() {}
+#ifdef BEAGLE
+    char m_lcd_buf[LCD_BUF_SIZE];
+    int m_lcd_len;
+    int m_lcd_fd;
+
+    void lcd_message(const char *fmt, ...);
+    void lcd_clear_screen();
+    void lcd_go_home();
+    void lcd_clear_and_home();
+#endif
 };
--- lcd/lcd_beagle.cpp
+++ lcd/lcd_beagle.cpp
@@ -1,21 +1,61 @@
 #include "lcd.h"
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 using std::cerr;
 using std::endl;
 
 void LCD::initialize()
 {
-    // TODO
+    m_lcd_fd = open("/dev/lcd0", O_WRONLY);
+    if (m_lcd_fd < 0) {
+        perror("Error on opening LCD: ");
+        exit(1);
+    }
 }
 
 void LCD::finalize()
 {
-    // TODO
+    close(m_lcd_fd);
+}
+
+void LCD::lcd_message(const char* fmt, ...)
+{
+    va_list vl; 
+    va_start(vl, fmt);
+    m_lcd_len = vsnprintf(m_lcd_buf, sizeof(m_lcd_buf), fmt, vl);
+    va_end(vl);
+    write(m_lcd_fd, m_lcd_buf, m_lcd_len);
+}
+
+void LCD::lcd_clear_screen()
+{
+    lcd_message("\033[2J");
+}
+
+void LCD::lcd_go_home()
+{
+    lcd_message("\033[H");
+}
+
+void LCD::lcd_clear_and_home()
+{
+    lcd_clear_screen();
+    lcd_go_home();
 }
 
 void LCD::displayBoolArray8(bool *values)
 {
-    // TODO
+    lcd_clear_and_home();
+
+    for (int i = 0; i < 8; ++i){
+        lcd_message("%d", (int)values[i]);
+    }
 }
